19aac412b04ca07951e8f373c0dfbb6dad0ab567
Allen Shamrock <allenshamrock37@gmail.com>
2025-01-21 16:41:27 +0300

    {Feat}:Implemented add review feature

--- Products/urls.py
+++ Products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('category/<slug:category_slug>/product/<slug:product_slug>/', views.product_detail, name='product_detail')
-
+   path('category/<slug:category_slug>/product/<slug:product_slug>/', views.product_detail, name='product_detail'),
+   path('submit_review/<int:product_id>/', views.submit_review, name='submit_review')
 ]
--- Products/views.py
+++ Products/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Product,VariationCategory,Variation,ReviewRating,ProductGallery
+from django.conf import settings
+from django.urls import reverse 
+from .forms import ReviewForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 # Create your views here.
 def product_detail(request,category_slug,product_slug):
     try:
@@ -51,3 +58,57 @@ def product_detail(request,category_slug,product_slug):
     print(context)
 
     return render(request ,'products/product_detail.html',context)
+
+#{Security guard for our website} Validates the url if it comes from our allowed hosts
+def is_safe_url(url,allowed_hosts):
+    from urllib.parse import urlparse
+    url = url.strip()
+    if url == '':
+        return False
+    url_obj = urlparse(url)
+    return url_obj.netloc in allowed_hosts
+
+    """
+   This decorator allows only login user or registered users to leave a review
+    """
+@login_required  
+def submit_review(request,product_id):
+    # Get product instance to retrieve the category slug & product slug
+    product = get_object_or_404(Product,id=product_id)
+    category_slug = product.category.slug
+    product_slug = product.slug
+
+    # Get the referer URL or fallback to the product detail page
+    referer_url = request.META.get('HTTP_REFERER',reverse('product_detail',args=[category_slug,product_slug]))
+
+    # Validate the referer URL to ensure its safe
+    if not is_safe_url(referer_url, allowed_hosts={request.get_host()}):
+        referer_url = reverse('product_detail',args=[category_slug,product_slug]) 
+    
+    # If this is a Post request we need to process the from data
+    if request.method == 'POST':
+        # Create a form instance and populate it with data from the request
+        form = ReviewForm(request.POST)
+        # Check whether its valid
+        if form.is_valid():
+            review,created = ReviewRating.objects.update_or_create(
+            user = request.user,
+            product_id = product_id,
+            defaults={
+                'subject':form.cleaned_data['subject'],
+                'rating':form.cleaned_data['rating'],
+                'subject':form.cleaned_data['subject'],
+                'ip':request.META.get('REMOTE_ADDR')
+            }
+            )
+
+            if created:
+                messages.success(request,'Thank you!, Your review has been submitted')
+            
+            else:
+                messages.success(request, 'Thank you!,your review has been updated')
+        else:
+            messages.error(request, 'There was an error with your submission')
+            return redirect(referer_url)
+    
+    return redirect(referer_url)
